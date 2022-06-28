cf24e45f8522df0889c7a1aa64bb2fc1ad7b40ff
kotkiller <kotkiller@inbox.ru>
2022-06-28 18:16:43 +0300

    update:tests finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n<0){
+        return (Math.sign(n)*parseInt(n.toString().split('').reverse().join('')))*-1;
+    }
+    else {
+        return Math.sign(n)*parseInt(n.toString().split('').reverse().join(''));
+    }
+             }
+
+
